a32aee69f23886254e692622aaaa47eb476b2bc1
Jason Walsh <jwalsh@bluekai.com>
2013-12-03 10:19:25 -0800

    Simulate multiple providers reading from digitalData

--- src/digitalanalytics.js
+++ src/digitalanalytics.js
@@ -1,16 +1,16 @@
 // This extends the data in the digitalData object with utility
-// functions 
-// TODO: Confirm the param addition that allow for object
-// destructuring 
+// functions
 
-// This is the generic wrapper required based on the event handling
-// library used on the client site
+// Analytics Provider 1
 $(document).on(
-  "digitalData", 
-  function(event, p1) { 
-    bk_allow_multiple_calls = true;
-    BKTAG
-      ._reset()
-      .addCtxParam(p1)
-      .doTag(2);
+  "digitalData",
+  function(event, p1) {
+    console.log('Provider 1', event, p1);
+});
+
+// Analytics Provider 1
+$(document).on(
+  "digitalData",
+  function(event, p1) {
+    console.log('Provider 2', event, p1);
 });
